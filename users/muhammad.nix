0ad87cd0a8da528efce067574087310c6d7293a7
Muhammad <akhmedibirov.management@gmail.com>
2026-07-22 14:40:39 +0300

    Configure declarative user password

--- users/muhammad.nix
+++ users/muhammad.nix
@@ -1,11 +1,16 @@
 { pkgs, ... }:
 
 {
+  users.mutableUsers = false;
+
+
   users.users.muhammad = {
     isNormalUser = true;
     home = "/home/muhammad";
     extraGroups = [ "wheel" ];
     shell = pkgs.bashInteractive;
+
+    hashedPassword = "$6$42pkKA9Gi7LONNJr$jPxO/fnkKv4nQPJVqtIhe5IFRAnKkBUG7qwFY1O.kweF4FinfRRWZqHyJE0tzFMRLo9ro9Wq58GujeZ4BMILM1";
 
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK4PiqguA69CqKLAsUUisiffwAYFVZIyT5wRzaa1+y1j muhammad@nixos-vm"
